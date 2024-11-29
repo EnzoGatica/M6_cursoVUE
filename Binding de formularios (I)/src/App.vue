8f692e036c7bf930b0e1268784cffe9a355ea92b
EnzoGatica <enzogatica96@gmail.com>
2024-11-29 18:37:03 -0300

    Corrección
    actividad correcta

--- Binding de formularios (I)/src/App.vue
+++ Binding de formularios (I)/src/App.vue
@@ -1,97 +1,172 @@
-<template>
-    <div class="carnet">
-      <aside>
-        <div>
-          <span>CEDULA DE INDENTIDAD</span>
-          <img width="40" src="/chile_bandera.png" alt="" />
-        </div>
+<script>
+  import chip1 from './assets/chip1.png';
+  import chip2 from './assets/chip2.png';
+  import chip3 from './assets/chip3.png';
+  import tipo1 from './assets/visa.png';
+  import tipo2 from './assets/master.png';
+  import tipo3 from './assets/american.png';
 
-        <img src="/foto_elon.jpg" alt="" />
-        <p>RUN {{ run }}</p>
-      </aside>
 
-      <main>
-        <header>
-          <p>REPUBLICA DE CHILE</p>
-          <span>SERVICIO DE REGISTRO CIVIL E IDENTIFICACIÓN</span>
-        </header>
-        <section>
-        <div class="grid-2-colums">
-          <article>
-            <header>APELLIDOS</header>
-            <span>{{ apellidos }}</span>
-          </article>
-        </div>
-        <div class="grid-2-colums">
-          <article>
-            <header>NOMBRES</header>
-            <span>{{ nombres }}</span>
-          </article>
-        </div>
-          <div class="grid-2-colums">
-            <article>
-              <header>NACIONALIDAD</header>
-              <span>{{ nacionalidad }}</span>
-            </article>
-            <article>
-              <header>SEXO</header>
-              <span>{{ sexo }}</span>
-            </article>
-          </div>
-          <div class="grid-2-colums">
-            <article>
-              <header>FECHA DE NACIMIENTO</header>
-              <span>{{ fechaNacimiento }}</span>
-            </article>
-            <article>
-              <header>NÙMERO DOCUMENTO</header>
-              <span>{{ numeroDocumento }}</span>
-            </article>
-          </div>
-          <div class="grid-2-colums">
-            <article>
-              <header>FECHA DE EMISIÒN</header>
-              <span>{{ fechaEmision }}</span>
-            </article>
-            <article>
-              <header>FECHA DE VENCIMIENTO</header>
-              <span>{{ fechaVencimiento }}</span>
-            </article>
-          </div>
-          <div class="grid-2-colums">
-          <article>
-            <header>FIRMA DEL TITULAR</header>
-            <span>{{ firma }}</span>
-          </article>
-        </div>
-        </section>
-      </main>
-    </div>
-  </template>
-  
-  <script>
-  import chileBandera from '/chile_bandera.png';
-  import fotoElon from '/foto_elon.jpg';
-  
-  export default {
-    data() {
-      return {
-        run: '22.222.222-2',
-        apellidos: 'Musk',
-        nombres: 'Elon Reeve',
-        nacionalidad: 'Sudafricano',
-        sexo: 'M',
-        fechaNacimiento: '28 JUN 1971',
-        numeroDocumento: '222.222.222',
-        fechaEmision: '01 ENE 2025',
-        fechaVencimiento: '01 ENE 2030',
-        firma: 'X Æ A-12',
-        chileBandera,
-        fotoElon
-      };
-    }
+  export default { name: "App", 
+  data() { 
+    return { 
+      tarjeta: { 
+        tituloTarjeta: "",
+        chipImagenTarjeta: {chip1, chip2, chip3},
+        numeroTarjeta: "", 
+        fechaExpiraTarjeta: "", 
+        nombreClienteTarjeta: "", 
+        tipoSrcTarjeta: {tipo1, tipo2, tipo3}
+        }, 
+      }; 
+    }, 
   };
-  </script>
+
+</script>
+
+<template>
+    <div id="app">
+      <form>
+        <div>
+          <label>Título de la tarjeta: </label>
+          <select v-model="tarjeta.tituloTarjeta">
+            <option  >Visa Clasica Nacional</option>
+            <option  >Visa Golden Nacional</option>
+            <option >Visa International</option>
+            <option  >Visa Black</option>
+            <option  >Visa Master-Card</option>
+            <option  >Visa American-Express</option>
+          </select>
+        </div>
+
+        <div>
+          <label>Chip SRC: </label>
+          <select v-model="tarjeta.chipImagenTarjeta">
+            <option :value="tarjeta.chipImagenTarjeta.chip1" >Chip 1</option>
+            <option :value="tarjeta.chipImagenTarjeta.chip2" >Chip 2</option>
+            <option :value="tarjeta.chipImagenTarjeta.chip3" >Chip 3</option>
+          </select>
+        </div>
+
+        <div>
+          <label>Número: </label>
+          <input type="number" v-model="tarjeta.numeroTarjeta"/>
+        </div>
+
+        <div>
+          <label>Fecha de expiración: </label>
+          <input type="month" v-model="tarjeta.fechaExpiraTarjeta"/>
+          
+        </div>
+
+        <div>
+          <label>Propietario: </label>
+          <input type="text" v-model="tarjeta.nombreClienteTarjeta"/>
+        </div>
+
+        <div>
+          <label>Tipo de tarjeta SRC: </label>
+          <select v-model="tarjeta.tipoSrcTarjeta">
+            <option :value="tarjeta.tipoSrcTarjeta.tipo1">Visa</option>
+            <option :value="tarjeta.tipoSrcTarjeta.tipo2">Master Card</option>
+            <option :value="tarjeta.tipoSrcTarjeta.tipo3">American Express</option>
+          </select>
+        </div>
+      </form>
+
+      <div class="carnet">
+        <h3>{{tarjeta.tituloTarjeta}}</h3>
+        <img width="40" :src="tarjeta.chipImagenTarjeta" alt="" />
+        <div>
+          <h2>{{tarjeta.numeroTarjeta}}</h2>
+          <span>Fecha Exp: <b>{{tarjeta.fechaExpiraTarjeta}}</b></span>
+        </div>
+        <footer>
+          <span>{{tarjeta.nombreClienteTarjeta}}</span>
+          <img :src="tarjeta.tipoSrcTarjeta" width="60" />
+        </footer>
+      </div>
+    </div>
+</template>
+
+<style>
+  * {
+    margin: 0;
+  }
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 60vh;
+    font-size: 10px;
+    font-family: sans-serif;
+  }
+
+  #app {
+    display: flex;
+    gap: 40px;
+  }
+  form {
+    padding: 18px;
+    border-radius: 20px;
+    border: ridge 1px;
+    box-shadow: 1px 1px 1px 1px gray;
+    font-weight: bold;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    justify-content: center;
+  }
+  form div {
+    display: grid;
+    grid-template-columns: 100px 200px;
+    align-items: center;
+    gap: 5px;
+  }
+
+  form div label {
+    text-align: right;
+  }
+
+  .carnet {
+    width: 400px;
+    height: 230px;
+    background: #1c1a1e;
+    color: white;
+    border-radius: 20px;
+    padding: 20px;
+    box-shadow: 0px 0px 10px 2px black;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  h3 {
+    font-size: 20px;
+  }
+
+  h2 {
+    font-size: 30px;
+    letter-spacing: 10px;
+    margin-bottom: 15px;
+    text-shadow: 0px 0px 1px gold;
+    font-family: Times;
+  }
+
+  b {
+    font-size: 16px;
+  }
+
+  footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    font-weight: bold;
+    font-size: 18px;
+    text-transform: uppercase;
+    font-style: italic;
+  }
+</style>
   
 
-  
+
